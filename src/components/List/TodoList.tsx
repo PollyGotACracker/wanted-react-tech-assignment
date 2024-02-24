9809b7f5966cca1c2fcb94dce0db767626d0ca15
PollyGotACracker <bitterns96@gmail.com>
2024-02-24 23:27:37 +0900

    feat: Todo 애니메이션 적용

--- src/components/List/TodoList.tsx
+++ src/components/List/TodoList.tsx
@@ -2,17 +2,34 @@ import style from "./todoList.module.css";
 import TodoItem from "./TodoItem";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function TodoList() {
   const list = useSelector((state: RootState) => state.todo.list);
 
+  const motionProps = {
+    initial: "hidden",
+    animate: "visible",
+    variants: {
+      hidden: { opacity: 1 },
+      visible: {
+        opacity: 1,
+        transition: {
+          staggerChildren: 0.2,
+        },
+      },
+    },
+  };
+
   return (
     <section className={style.wrapper}>
-      <ul className={style.list}>
-        {list.map(({ id, content }) => (
-          <TodoItem key={id} id={id} content={content} />
-        ))}
-      </ul>
+      <motion.ul className={style.list} {...motionProps}>
+        <AnimatePresence mode="sync">
+          {list.map(({ id, content }) => (
+            <TodoItem key={id} id={id} content={content} />
+          ))}
+        </AnimatePresence>
+      </motion.ul>
     </section>
   );
 }
